7a1ec57e59289c754532fad9a9c0ddf07bbc67cc
masha-mtc <107871776+masha-mtc@users.noreply.github.com>
2022-06-27 11:55:50 +0600

    plants

---
+++ Calculate/Plant/plantMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate.Plant
+{
+
+    public interface ITwoArgumentsCalculator
+    {
+        double Calculate(double argumentOne, double ArgumentTwo);
+    }
+    public class SubtractionCalculator : ITwoArgumentsCalculator
+    {
+        public double Calculate(double firstArg, double secondArg) => firstArg - secondArg;
+    }
+    public class ZeroCalculator : ITwoArgumentsCalculator
+    {
+        public double Calculate(double firstArg, double secondArg) => 0;
+    }
+    static class TwoArgPlant
+    {
+        internal static ITwoArgumentsCalculator CreateCalculator(string Calc_name)
+        {
+            if(Calc_name == "substract")
+            {
+                return new SubtractionCalculator();
+            }
+            else
+            {
+                return new ZeroCalculator();
+            }
+        }
+
+    }
+    
+}
